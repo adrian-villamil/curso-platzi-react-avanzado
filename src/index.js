ebcc4478a635fabb0eb8b9f3a4a2d169f51d2f7a
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-03-28 14:11:59 -0500

    Importamos react y react-dom para crear el root del proyecto, el cuál será el div con id app.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Mi proyecto con react avanzado');
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<h1>Seguimos con el curso avanzado!</h1>);
