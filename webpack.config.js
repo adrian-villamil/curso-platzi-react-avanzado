d6f53ce25d8dbe0a119dcd24bb71479bf504210b
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-03-28 14:11:25 -0500

    Le indicamos que el template que vamos a utilizar es el index.html que creamos en el src, además incluimos el babel-loader para que transpile todos los archivos js y los presets para js y react.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,27 @@ module.exports = {
   output: {
     filename: 'app.bundle.js'
   },
+  mode: 'development',
   plugins: [
-    new HtmlWebpackPlugin()
-  ]
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  }
 }
