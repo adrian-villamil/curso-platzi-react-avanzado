c9ca0ec82f05a5210b9997a09e364621cbe3f2df
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-02 20:25:01 -0500

    Agregamos dos efectos, uno para hacer llamado a la api y nos traiga las categorías, y el otro para que nos muestre otra lista de categorías fijada en la parte superior después de hacer scroll de 200 pixeles.

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Category } from "../Category";
 import { Item, List } from "./styles";
-import data from '../../../api/db.json';
+import axios from "axios";
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
+
+  useEffect(() => {
+    async function getCategories() {
+      try {
+        const response = await axios.get('https://petgram-server-edsf8xpy2.now.sh/categories');
+        setCategories(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getCategories();
+  }, []);
+
+  useEffect(() => {
+    const onScroll = (event) => {
+      window.scrollY > 200 ? setShowFixed(true) : setShowFixed(false);
+    };
+
+    document.addEventListener('scroll', onScroll);
+
+    return () => document.removeEventListener('scroll', onScroll);
+  }, [showFixed]);
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
-        data.categories.map(category => (
+        categories.map(category => (
           <Item key={category.id}>
             <Category {...category} />
           </Item>
         ))
       }
     </List>
+  );
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   );
 };
