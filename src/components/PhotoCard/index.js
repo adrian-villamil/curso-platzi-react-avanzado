2370c214cc1db469a149933dfdbc31bf2f43c3b0
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-04 10:07:05 -0500

    Hicimos un lazy loading para mejorar el performance de la app.

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,19 +1,39 @@
-import React from "react";
-import { Button, Img, ImgWrapper } from "./styles";
+import React, { useEffect, useRef, useState } from "react";
+import { Article, Button, Img, ImgWrapper } from "./styles";
 import { MdFavoriteBorder } from 'react-icons/md';
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const [show, setShow] = useState(false);
+  const element = useRef(null);
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver(function (entries) {
+      const { isIntersecting } = entries[0];
+      if (isIntersecting) {
+        console.log('Si');
+        setShow(true);
+        observer.disconnect();
+      }
+    });
+    observer.observe(element.current);
+  }, [element]);
+
   return (
-    <article>
-      <a href={`/default/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size={32} />{likes} likes!
-      </Button>
-    </article>
+    <Article ref={element}>
+      {
+        show &&
+        <>
+          <a href={`/default/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size={32} />{likes} likes!
+          </Button>
+        </>
+      }
+    </Article>
   );
 };
