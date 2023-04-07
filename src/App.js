89dcd65ed3990d91b7a07115e5b353e529432f7c
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-07 01:36:22 -0500

    Agregamos una condición ternearia para que muestre una sóla foto si existe un query params dentro de la url.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import { ListOfCategories } from "./components/ListOfCategories";
 import { ListOfPhotoCards } from "./components/ListOfPhotoCards";
 import { Logo } from "./components/Logo";
 import { GlobalStyles } from "./styles/GlobalStyles";
+import { PhotoCardWithQuery } from "./container/PhotoCardWithQuery";
 
 export const App = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const detailId = urlParams.get('detail');
+
   return (
     <>
       <GlobalStyles />
       <Logo />
-      <ListOfCategories />
-      <ListOfPhotoCards categoryId={4} />
+      {
+        detailId
+        ? <PhotoCardWithQuery id={detailId} />
+        : <>
+          <ListOfCategories />
+          <ListOfPhotoCards categoryId={1} />
+        </>
+      }
     </>
   );
 };
