9c678e4274faabcc76f025e41e3e618d11e73d9f
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-07 01:35:13 -0500

    Modificamos la ruta para que tenga un query param.

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -17,7 +17,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
       {
         show &&
         <>
-          <a href={`/default/${id}`}>
+          <a href={`/?detail=${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
