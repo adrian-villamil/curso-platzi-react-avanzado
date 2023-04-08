5a8794cf28db5c6cf718f2b1deb83a14ac847067
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-07 19:18:44 -0500

    Creamos una nueva página que tendrá los detalles de una sola publicación.

---
+++ src/pages/Detail.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { PhotoCardWithQuery } from "../container/PhotoCardWithQuery";
+
+export const Detail = () => {
+  const { detailId } = useParams();
+
+  return (
+    <PhotoCardWithQuery id={detailId} />
+  );
+};
