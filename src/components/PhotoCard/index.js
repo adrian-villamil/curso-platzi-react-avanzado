80c2b802579c8d3789fdf322b7b02a483488b110
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-07 19:19:27 -0500

    Reemplazamos el anchor por un Link y modificamos el path para que se diriga a la página de detalles.

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -3,6 +3,7 @@ import { Article, Img, ImgWrapper } from "./styles";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { useNearScreen } from "../../hooks/useNearScreen";
 import { FavButton } from "../FavButton";
+import { Link } from "react-router-dom";
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
@@ -15,11 +16,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
       {
         show &&
         <>
-          <a href={`/?detail=${id}`}>
+          <Link to={`/detail/${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
-          </a>
+          </Link>
           <FavButton id={id} liked={liked} likes={likes} setLiked={setLiked} />
         </>
       }
