2d47a225c10930f83cda1e5282d7e129d4f4b145
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-10 18:59:08 -0500

    Añadimos el formulario a la página NoRegisteredUser.

--- src/pages/NoRegisteredUser.js
+++ src/pages/NoRegisteredUser.js
@@ -1,13 +1,11 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
+import { UserForm } from "../components/UserForm";
 
 export const NoRegisteredUser = () => {
   const { activateAuth } = useContext(AppContext);
 
   return (
-    <form onSubmit={activateAuth}>
-      <label htmlFor="">Iniciar sesión</label>
-      <input type="submit" value='Iniciar Sesión' />
-    </form>
+    <UserForm onSubmit={activateAuth} />
   );
 };
