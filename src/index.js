7fd89b46aa5d9a53046f8c712291923f010c1314
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-09 21:40:39 -0500

    Agregamos el provider a la app.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import { App } from "./App";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { AppProvider } from "./context/AppContext";
 
 const client = new ApolloClient({
   uri: 'https://petgram-server-edsf8xpy2.now.sh/graphql',
@@ -10,7 +11,9 @@ const client = new ApolloClient({
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <AppProvider>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </AppProvider>
 );
