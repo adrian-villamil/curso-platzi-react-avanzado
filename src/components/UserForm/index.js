172da5007d42af0ed78ea55eef4ff805bef90f44
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-10 20:31:37 -0500

    Creamos estilos para el formulario y sus elementos.

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { useInputs } from "../../hooks/useInputs";
+import { Button, Form, Input, Title } from "./styles";
 
-export const UserForm = ({ onSubmit }) => {
+export const UserForm = ({ onSubmit, title }) => {
   const email = useInputs('');
   const password = useInputs('');
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="email"
-        placeholder="Email"
-        {...email}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        {...password}
-      />
-      <input type="submit" id="submit" value='Iniciar Sesión' />
-    </form>
+    <>
+      <Title>{title}</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          type="email"
+          placeholder="Email"
+          {...email}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          {...password}
+        />
+        <Button>{title}</Button>
+      </Form>
+    </>
   );
 };
