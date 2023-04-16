4d1fe929741084fc7244fa446611aa904e25e386
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-15 23:21:44 -0500

    Creamos la mutación para registrar usuarios.

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,15 +1,38 @@
 import React from "react";
 import { useInputs } from "../../hooks/useInputs";
 import { Button, Form, Input, Title } from "./styles";
+import { gql, useMutation } from "@apollo/client";
+
+const REGISTER = gql`
+  mutation signup($input: UserCredentials!) {
+    signup(input: $input)
+  }
+`;
 
 export const UserForm = ({ onSubmit, title }) => {
+  const [registerUser, { data, loading, error }] = useMutation(REGISTER);
   const email = useInputs('');
   const password = useInputs('');
+
+  if (loading) return <p>Submitting...</p>;
+  if (error) return <p>Submission error: {error.message}</p>
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    registerUser({
+      variables: {
+        input: {
+          email: email.value,
+          password: password.value
+        }
+      }
+    }).then(onSubmit);
+  };
 
   return (
     <>
       <Title>{title}</Title>
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={handleSubmit}>
         <Input
           type="email"
           placeholder="Email"
@@ -20,7 +43,7 @@ export const UserForm = ({ onSubmit, title }) => {
           placeholder="Password"
           {...password}
         />
-        <Button>{title}</Button>
+        <Button type="submit">{title}</Button>
       </Form>
     </>
   );
