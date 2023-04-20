37238dbf71e1a18a8f8d17e9d153f7a74ca93af5
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-19 23:17:53 -0500

    Creamos un header de autorización que enviaremos al servidor de graphql con el token de sesión.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import { App } from "./App";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
 import { AppProvider } from "./context/AppContext";
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'https://petgram-server-edsf8xpy2.now.sh/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = window.sessionStorage.getItem('token');
+  
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+});
 
 const client = new ApolloClient({
-  uri: 'https://petgram-server-edsf8xpy2.now.sh/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
