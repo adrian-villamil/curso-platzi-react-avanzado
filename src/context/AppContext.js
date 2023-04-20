ae4da31f1a69c96458f00ff7bee3faa6b81b7f9a
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-19 19:19:04 -0500

    Guardamos el token de la sesión para hacer persistencia de este.

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -3,10 +3,13 @@ import React, { useState } from "react";
 export const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(() => {
+    return window.sessionStorage.getItem('token');
+  });
 
-  const activateAuth = () => {
+  const activateAuth = (token) => {
     setIsAuth(true);
+    window.sessionStorage.setItem('token', token);
   };
 
   return (
