c607cccde7238fcb29ca46c56dd9a58b3b734a72
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-20 00:21:40 -0500

    En la página de favoritos traemos los post que les ha dado like por el usuario, llamando una lista de los post.

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { ListOfFavs } from "../components/ListOfFavs";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_FAVS = gql`
+  query GetFavs {
+    favs {
+      id
+      categoryId
+      src
+      likes
+      userId
+    }
+  }
+`;
 
 export const Favs = () => {
+  const { loading, error, data } = useQuery(GET_FAVS, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+
   return (
-    <h1>Favs</h1>
+    <>
+      <ListOfFavs favs={data.favs} />
+    </>
   );
 };
