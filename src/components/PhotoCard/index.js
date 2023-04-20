8a9e584b80a3e5a13e87b7759d0e32ba09e26db7
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-19 23:19:23 -0500

    Ya no necesitamos el local storage para conocer cuantos me gusta tiene una publicación, sólo lo recibimos en las props del componente.

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { Article, Img, ImgWrapper } from "./styles";
-import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { useNearScreen } from "../../hooks/useNearScreen";
 import { FavButton } from "../FavButton";
 import { Link } from "react-router-dom";
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `like-${id}`;
-  const [liked, setLiked] = useLocalStorage(key, false);
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen();
 
   return (
@@ -21,7 +18,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} />
             </ImgWrapper>
           </Link>
-          <FavButton id={id} liked={liked} likes={likes} setLiked={setLiked} />
+          <FavButton id={id} liked={liked} likes={likes} />
         </>
       }
     </Article>
