333aed11bfc3095998a31a33439dbe48e995815c
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-22 17:53:24 -0500

    Agregamos la funcionalidad de cerrar sesión.

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,3 +1,4 @@
+import { useApolloClient } from "@apollo/client";
 import React, { useState } from "react";
 
 export const AppContext = React.createContext();
@@ -6,14 +7,21 @@ export const AppProvider = ({ children }) => {
   const [isAuth, setIsAuth] = useState(() => {
     return window.sessionStorage.getItem('token');
   });
+  const client = useApolloClient();
 
   const activateAuth = (token) => {
     setIsAuth(true);
     window.sessionStorage.setItem('token', token);
   };
 
+  const removeAuth = () => {
+    setIsAuth(false);
+    window.sessionStorage.removeItem('token');
+    client.resetStore();
+  };
+
   return (
-    <AppContext.Provider value={{ isAuth, activateAuth }}>
+    <AppContext.Provider value={{ isAuth, activateAuth, removeAuth }}>
       {children}
     </AppContext.Provider>
   );
