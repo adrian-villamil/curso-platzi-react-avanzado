72a9fd03647e9986d5968326999d72810b14d83a
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-22 23:11:30 -0500

    Modificamos el título y agregamos un meta data con helmet.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,17 @@ import React from "react";
 import { ListOfCategories } from "../components/ListOfCategories";
 import { ListOfPhotoCards } from "../components/ListOfPhotoCards";
 import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 export const Home = () => {
   const { categoryId } = useParams();
 
   return (
     <>
+      <Helmet>
+        <title>Petgram - Tu app de fotos de mascotas</title>
+        <meta name="description" content="Con Petgram puedes encontrar fotos de animales domésticos muy bonitos" />
+      </Helmet>
       <ListOfCategories />
       <ListOfPhotoCards categoryId={categoryId} />
     </>
