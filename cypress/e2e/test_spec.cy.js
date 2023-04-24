7257cfe6c9c59bd9816339702cd0afeb808985a0
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-04-24 02:01:10 -0500

    Mi primer test con cypress.

---
+++ cypress/e2e/test_spec.cy.js
@@ -0,0 +1,21 @@
+describe('Mi primer test', () => {
+  it('para ver si la app funciona', () => {
+    cy.visit('/')
+  })
+
+  it('navegamos a una categoría y vemos fotos', () => {
+    cy.visit('/pet/1')
+    cy.visit('article')
+  })
+
+  it('si podemos navegar con la navbar a la home', () => {
+    cy.visit('/pet/1')
+    cy.get('nav a').first().click()
+    cy.url().should('include', '/')
+  })
+
+  it('los usuarios no registrados ven el formulario de registro e inicio de sesión al ir a favs', () => {
+    cy.visit('/favs')
+    cy.get('form').should('have.length', 2)
+  })
+})
